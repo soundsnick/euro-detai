c338f86a703441a2873c41e58c0aa0041900c8a2
soundsnick <soundsnick@gmail.com>
2019-05-07 17:33:07 +0600

    asd

--- app/mailers/default_mailer.rb
+++ app/mailers/default_mailer.rb
@@ -3,21 +3,21 @@ class DefaultMailer < ApplicationMailer
 
   def query_email(query)
     @query = query
-    mail(to: "soundsnick@gmail.com", subject: 'Узнать стоимость ДВС, КПП...')
+    mail(to: "detal911@yandex.ru", subject: 'Узнать стоимость ДВС, КПП...')
   end
 
   def comment_email(comment)
     @comment = comment
-    mail(to: "soundsnick@gmail.com", subject: "Добавлен новый комментарий от пользователя #{@comment.name}")
+    mail(to: "detal911@yandex.ru", subject: "Добавлен новый комментарий от пользователя #{@comment.name}")
   end
 
   def feedback_email(feedback)
     @comment = feedback
-    mail(to: "soundsnick@gmail.com", subject: "Добавлен новый отзыв от пользователя #{@comment.name}")
+    mail(to: "detal911@yandex.ru", subject: "Добавлен новый отзыв от пользователя #{@comment.name}")
   end
 
   def order_email(order)
     @order = order
-    mail(to: "soundsnick@gmail.com", subject: "Новый заказ от пользователя #{@order.name}")
+    mail(to: "detal911@yandex.ru", subject: "Новый заказ от пользователя #{@order.name}")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
