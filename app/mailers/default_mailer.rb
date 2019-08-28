07c96748d41662f372519595ef6d208a1968c2e0
soundsnick <soundsnick@gmail.com>
2019-08-29 00:24:33 +0600

    asd

--- app/mailers/default_mailer.rb
+++ app/mailers/default_mailer.rb
@@ -1,24 +1,23 @@
 class DefaultMailer < ApplicationMailer
   default from: "detal.ev@yandex.ru"
-  @to = "yernazar@askarovich.kz"
 
   def query_email(query)
     @query = query
-    mail(to: @to, subject: 'Узнать стоимость ДВС, КПП...')
+    mail(to: "yernazar@askarovich.kz", subject: 'Узнать стоимость ДВС, КПП...')
   end
 
   def comment_email(comment)
     @comment = comment
-    mail(to: @to, subject: "Добавлен новый комментарий от пользователя #{@comment.name}")
+    mail(to: "yernazar@askarovich.kz", subject: "Добавлен новый комментарий от пользователя #{@comment.name}")
   end
 
   def feedback_email(feedback)
     @comment = feedback
-    mail(to: @to, subject: "Добавлен новый отзыв от пользователя #{@comment.name}")
+    mail(to: "yernazar@askarovich.kz", subject: "Добавлен новый отзыв от пользователя #{@comment.name}")
   end
 
   def order_email(order)
     @order = order
-    mail(to: @to, subject: "Новый заказ от пользователя #{@order.name}")
+    mail(to: "yernazar@askarovich.kz", subject: "Новый заказ от пользователя #{@order.name}")
   end
 end
