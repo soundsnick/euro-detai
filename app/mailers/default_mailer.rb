f4089bda4b6388824652a11c7d584d9ca2bdd198
soundsnick <soundsnick@gmail.com>
2019-08-29 00:29:24 +0600

    asd

--- app/mailers/default_mailer.rb
+++ app/mailers/default_mailer.rb
@@ -3,21 +3,21 @@ class DefaultMailer < ApplicationMailer
 
   def query_email(query)
     @query = query
-    mail(to: "yernazar@askarovich.kz", subject: 'Узнать стоимость ДВС, КПП...')
+    mail(to: "detal911@yandex.ru", subject: 'Узнать стоимость ДВС, КПП...')
   end
 
   def comment_email(comment)
     @comment = comment
-    mail(to: "yernazar@askarovich.kz", subject: "Добавлен новый комментарий от пользователя #{@comment.name}")
+    mail(to: "detal911@yandex.ru", subject: "Добавлен новый комментарий от пользователя #{@comment.name}")
   end
 
   def feedback_email(feedback)
     @comment = feedback
-    mail(to: "yernazar@askarovich.kz", subject: "Добавлен новый отзыв от пользователя #{@comment.name}")
+    mail(to: "detal911@yandex.ru", subject: "Добавлен новый отзыв от пользователя #{@comment.name}")
   end
 
   def order_email(order)
     @order = order
-    mail(to: "yernazar@askarovich.kz", subject: "Новый заказ от пользователя #{@order.name}")
+    mail(to: "detal911@yandex.ru", subject: "Новый заказ от пользователя #{@order.name}")
   end
 end
